9a99a8f0a4eab36d87f3c02e55e3c79c976b9223
anand3693 <anandpbiju2004@gmail.com>
2025-12-09 13:16:04 +0530

    Intial commit

--- audio.py
+++ audio.py
@@ -4,20 +4,20 @@ import torch
 from faster_whisper import WhisperModel
 from pyannote.audio import Pipeline
 
-# Load environment variables
+
 load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 AUDIO_FILE = r"D:\Audio\LearningEnglishConversations-20251111-TheEnglishWeSpeakNoTwoWaysAboutIt.mp3"
 
-# 1️⃣ Load diarization model
+
 pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization-community-1",
     use_auth_token=HF_TOKEN
 )
 diarization = pipeline(AUDIO_FILE)
 
-# 2️⃣ Load Whisper model
+
 model = WhisperModel("large-v3", device="cuda" if torch.cuda.is_available() else "cpu")
 
 segments, _ = model.transcribe(AUDIO_FILE, beam_size=5, language=None)
@@ -26,8 +26,7 @@ results = []
 for seg in segments:
     text = seg.text
     start, end = seg.start, seg.end
-    
-    # Match with diarization speaker segments
+
     speaker = "Unknown"
     for turn, _, speaker_label in diarization.itertracks(yield_label=True):
         if start >= turn.start and end <= turn.end:
@@ -41,6 +40,5 @@ for seg in segments:
         "text": text
     })
 
-# Print formatted output
 for r in results:
     print(f"[{r['speaker']} - {r['start']:.2f}s]: {r['text']}")
